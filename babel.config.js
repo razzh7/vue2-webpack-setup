53b17876322196725586e512efa4e14cb24c9efe
razzh <razzhavenir@163.com>
2023-09-19 11:48:10 +0800

    chore: vue2 jsx preset

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: ['@babel/preset-env', '@vue/babel-preset-jsx'],
   plugins: [
-    'babel-plugin-transform-vue-jsx',
+    '@babel/plugin-transform-object-assign',
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-transform-runtime',
     '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-class-properties'
   ],
 }
